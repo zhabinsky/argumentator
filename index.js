12a0d9fc181ba1111879880cc16a1353ead97697
Vladislav Zhabinsky <vladislavzhabinsky@gmail.com>
2019-12-16 12:40:55 +0200

    submit

--- index.js
+++ index.js
@@ -1,62 +1,82 @@
-require ('console.table');
+const tableLogger = require ('console.table');
 const chalk = require ('chalk');
 
-const argumentator = userArguments => {
-  const argumentDeclarations = [
-    ...toArray (userArguments),
-    {
-      flags: `--manual`,
-      description: 'Shows all available CLI arguments',
-      action: () => table (argumentDeclarations) || process.exit (),
-    },
-  ];
-  const context = {};
-  const args = process.argv.slice (2);
+/**
+ * @param extpectedArg An array of configurations
+ * 	@param extpectedArg[i].flags comma separated flags
+ * 	@param extpectedArg[i].description text to be shown when showing --manual
+ * 	@param extpectedArg[i].value                an object to be merged with context
+ * 	@param extpectedArg[i].action should return an object to be merged with context
+ * 
+ * @param options An Object
+ * 
+ */
 
-  const paramRegex = /\-\-.*\=.*/;
-  const flagRegex = /^--/;
-
-  const extendContextFromConfig = argumentDeclaration => {
-    const contains = c => {
-      const arr = Array.isArray (c)
-        ? c.map (e => contains (e)).filter (e => !!e)
-        : args.filter (e => !paramRegex.test (e)).filter (e => e === c);
-      return arr.length > 0;
-    };
-    const {flags, action = () => ({}), value} = argumentDeclaration;
-    if (contains (flags.split (','))) {
-      const hasAction = action && typeof action === 'function';
-      const hasValue = value && typeof value === 'object';
-      if (hasAction) Object.assign (context, action (context) || {});
-      if (hasValue) Object.assign (context, value);
-    }
-  };
-  const extendContextWithValues = () => {
-    const params = args.filter (e => flagRegex.test (e));
-    params
-      .map (uprefixArgument)
-      .map (param => param.split ('='))
-      .map (([key, val = true]) => {
-        context[key] = val;
-      });
-  };
-  const collectTexts = () => {
-    context.texts = args.filter(e => !flagRegex.test(e));
+module.exports = function argumentator (expectedArgs, options = {}) {
+  if (!Array.isArray (expectedArgs)) {
+    /**
+     * Allow for single argument declaration to be passed
+     */
+    expectedArgs = [expectedArgs];
   }
 
-  collectTexts();
-  extendContextWithValues ();
-  argumentDeclarations.forEach (argumentDeclaration =>
-    extendContextFromConfig (argumentDeclaration)
-  );
-  context.args = args;
+  expectedArgs.push ({
+    flags: `--manual, --man`,
+    description: 'Shows all available CLI arguments',
+    action: () => {
+      /**
+	   * --manual flag will output all expected args
+	   * and quit the app
+	   */
+      logTable (expectedArgs);
+      process.exit ();
+    },
+  });
+
+  const context = {}; // initialise empty context object
+
+  /**
+   * Trying to retrieve args from options object
+   * if not, using process.argv.slice (2)
+   */
+  const {processArguments = process.argv.slice (2)} = options;
+
+  /**
+   * Gather all process args that are not flags
+   * and attach them to context as texts:[]
+   */
+  const flagRegex = /^--/;
+  context.texts = processArguments.filter (e => !flagRegex.test (e));
+
+  /**
+   * Add booleans to context 
+   * ex. flag "--copy" will extend context obj with {copy: true}
+   */
+  const params = processArguments.filter (e => flagRegex.test (e));
+  const unprefix = a => a.replace (/^\-\-/, '');
+  params.forEach (param => {
+    const [key, val = true] = unprefix (param).split ('=');
+    context[key] = val;
+  });
+
+  for (let i = 0; i < expectedArgs.length; i++) {
+    const extpectedArg = expectedArgs[i];
+    /**
+	 * If the argument is passed to process
+	 * Its value object and result from action()
+	 * will be appended to context object
+	 */
+    const argContext = buildContext (extpectedArg, processArguments);
+    Object.assign (context, argContext);
+  }
+
+  context.args = processArguments; // attaching process args to context
+
   return context;
 };
 
-module.exports = argumentator;
-
-function table (userArguments) {
-  const table = userArguments.map (item => {
+function logTable (args) {
+  const table = args.map (item => {
     const {flags = '?', description = '?'} = item;
     return {
       [chalk.blue ('Flags:')]: chalk.green (flags.split (',').join (', ')),
@@ -66,11 +86,32 @@ function table (userArguments) {
   console.table (table);
 }
 
-function toArray (a) {
-  if (Array.isArray (a)) return a;
-  return [a];
+const lengthMoreThanZero = e => e.trim ().length > 0;
+
+function buildContext (argument, args) {
+  const isInArgs = flag => args.indexOf (flag) !== -1;
+  const isMentioned = flags => flags.filter (isInArgs).length > 0;
+
+  const {flags, action = () => ({}), value} = argument;
+
+  const context = {};
+
+  const extendContext = (a = {}) => Object.assign (context, a);
+  const possibleFlags = flags.split (',').filter (lengthMoreThanZero);
+
+  const hasInArgs = isMentioned (possibleFlags);
+  if (!hasInArgs) return context;
+
+  /**
+   * Extending context depending with action():Object and value:Object
+   * these values come from an individual argument config
+   */
+  if (isType (action, 'function')) extendContext (action (context));
+  if (isType (value, 'object')) extendContext (value);
+
+  return context;
 }
 
-function uprefixArgument (a) {
-  return a.replace (/^\-\-/, '');
+function isType (value, type) {
+  return value && typeof value === type;
 }
